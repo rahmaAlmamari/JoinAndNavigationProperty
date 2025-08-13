1d8d781ef6343b3b105d1d6291dda7751f14872b
rahmaAlmamari <alr816125@gmail.com>
2025-08-13 11:51:57 +0400

    Set RDbContext as child for DbContext

--- JoinAndNavigationProperty/RDbContext.cs
+++ JoinAndNavigationProperty/RDbContext.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace JoinAndNavigationProperty
 {
-    public class RDbContext
+    public class RDbContext : DbContext
     {
     }
 }
