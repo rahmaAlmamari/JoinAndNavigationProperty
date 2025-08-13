c9b28595a63fe52816e7de5bf14d02d7925a38e7
rahmaAlmamari <alr816125@gmail.com>
2025-08-13 11:45:38 +0400

    Modify models commint

--- JoinAndNavigationProperty/Models/Department.cs
+++ JoinAndNavigationProperty/Models/Department.cs
@@ -17,9 +17,9 @@ namespace JoinAndNavigationProperty.Models
         [MaxLength(50)]
         public string DepartmentName { get; set; }
 
-        //One-to-many relationship with Student
-        public ICollection<Student> students { get; set; } // Navigation property to Student class
-        //One-to-One relationship with Manager
-        public Manager manager { get; set; } // Navigation property to Manager class
+        //One-to-many relationship with Student ...
+        public ICollection<Student> students { get; set; } // Navigation property to Student class ...
+        //One-to-One relationship with Manager ...
+        public Manager manager { get; set; } // Navigation property to Manager class ...
     }
 }
--- JoinAndNavigationProperty/Models/Manager.cs
+++ JoinAndNavigationProperty/Models/Manager.cs
@@ -17,9 +17,9 @@ namespace JoinAndNavigationProperty.Models
         [MaxLength(20)]
         public string ManagerName { get; set; }
 
-        // One-to-One relationship with Department
+        // One-to-One relationship with Department ...
         [ForeignKey("department")]
-        public int DepartmentId { get; set; } // Foreign key property for Department
-        public Department department { get; set; } // Navigation property to Department class
+        public int DepartmentId { get; set; } // Foreign key property for Department ...
+        public Department department { get; set; } // Navigation property to Department class ...
     }
 }
