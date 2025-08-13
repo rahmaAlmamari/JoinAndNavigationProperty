c86eab2ca948bcaa942865f181aa8aea94928f68
rahmaAlmamari <alr816125@gmail.com>
2025-08-13 12:13:44 +0400

    Extract IStudentRepository interface

---
+++ JoinAndNavigationProperty/Repositories/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using JoinAndNavigationProperty.Models;
+
+namespace JoinAndNavigationProperty.Repositories
+{
+    public interface IStudentRepository
+    {
+        void AddStudent(Student student);
+        void DeleteStudent(int id);
+        List<Student> GetAllStudents();
+        Student GetStudentById(int id);
+        void UpdateStudent(Student student);
+    }
+}
