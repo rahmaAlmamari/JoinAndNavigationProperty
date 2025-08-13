54bba8945d26e95aaec06718214ef670bba426b8
rahmaAlmamari <alr816125@gmail.com>
2025-08-13 11:08:07 +0400

    Add project files.

---
+++ JoinAndNavigationProperty/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JoinAndNavigationProperty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
