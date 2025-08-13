3bd4cb338456ad57e5eec5c4367f701e0d4eee1e
rahmaAlmamari <alr816125@gmail.com>
2025-08-13 11:29:25 +0400

    Adding Data annotations to Department

--- JoinAndNavigationProperty/Models/Department.cs
+++ JoinAndNavigationProperty/Models/Department.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +10,11 @@ namespace JoinAndNavigationProperty.Models
 {
     public class Department
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int DepartmentId { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string DepartmentName { get; set; }
     }
 }
