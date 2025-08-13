c2a0b20cba3b016af614bee51d7b7bdc497bdcbd
rahmaAlmamari <alr816125@gmail.com>
2025-08-13 11:28:39 +0400

    Adding Data annotations to Student class

--- JoinAndNavigationProperty/Models/Student.cs
+++ JoinAndNavigationProperty/Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,14 @@ namespace JoinAndNavigationProperty.Models
 {
     public class Student
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int StudentId { get; set; }
+        [Required]
+        [MaxLength(20)]
         public string StudentName { get; set; }
+        [Required]
+        [Range(0, 100)]
         public double StudentMark { get; set; }
     }
 }
