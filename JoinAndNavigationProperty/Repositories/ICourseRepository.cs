587e7d3cfa90ea67e2fa02ff37e34ed0c151788a
rahmaAlmamari <alr816125@gmail.com>
2025-08-13 12:26:28 +0400

    Extract ICourseRepository interface

---
+++ JoinAndNavigationProperty/Repositories/ICourseRepository.cs
@@ -0,0 +1,13 @@
+﻿using JoinAndNavigationProperty.Models;
+
+namespace JoinAndNavigationProperty.Repositories
+{
+    public interface ICourseRepository
+    {
+        void AddCourse(Course course);
+        void DeleteCourse(int id);
+        List<Course> GetAllCourses();
+        Course GetCourseById(int id);
+        void UpdateCourse(int id);
+    }
+}
