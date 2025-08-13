5730079acedb28fdbf2e2be256443d8661eb7b15
rahmaAlmamari <alr816125@gmail.com>
2025-08-13 12:18:09 +0400

    Extract IDepartmentRepository interface

---
+++ JoinAndNavigationProperty/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using JoinAndNavigationProperty.Models;
+
+namespace JoinAndNavigationProperty.Repositories
+{
+    public interface IDepartmentRepository
+    {
+        void AddDepartment(Department department);
+        void DeleteDepartment(int id);
+        List<Department> GetAllDepartments();
+        Department GetDepartmentById(int id);
+        void UpdateDepartment(Department department);
+    }
+}
