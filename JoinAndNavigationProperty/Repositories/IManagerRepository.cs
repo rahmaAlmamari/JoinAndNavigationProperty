45cd5a737f71c9dab2d6815dcdbcac77d601aa43
rahmaAlmamari <alr816125@gmail.com>
2025-08-13 12:23:30 +0400

    Extract IManagerRepository interface

---
+++ JoinAndNavigationProperty/Repositories/IManagerRepository.cs
@@ -0,0 +1,13 @@
+﻿using JoinAndNavigationProperty.Models;
+
+namespace JoinAndNavigationProperty.Repositories
+{
+    public interface IManagerRepository
+    {
+        void AddManager(Manager manager);
+        void DeleteManager(int id);
+        List<Manager> GetAllManagers();
+        Manager GetManagerById(int id);
+        void Updater(Manager manager);
+    }
+}
