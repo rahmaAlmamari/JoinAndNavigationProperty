6d18536e88687561a8f6cc0af16d64cf69f642b0
rahmaAlmamari <alr816125@gmail.com>
2025-08-13 11:54:37 +0400

    Adding DbSet properties for each entity

--- JoinAndNavigationProperty/RDbContext.cs
+++ JoinAndNavigationProperty/RDbContext.cs
@@ -4,15 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using JoinAndNavigationProperty.Models;
 
 namespace JoinAndNavigationProperty
 {
     public class RDbContext : DbContext
     {
+        //to override the OnModelCreating method to configure the relationships ...
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=LAPTOP-IUF9HHIH;Initial Catalog=StoreDB;Integrated Security=True;TrustServerCertificate=True");
         }
+        //to define the DbSet properties for each entity ...
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+
 
     }
 }
